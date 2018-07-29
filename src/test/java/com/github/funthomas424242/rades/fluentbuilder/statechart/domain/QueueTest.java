0caf8e412d812382cb29b03988f29c29ab4cf993
FunThomas424242 <funthomas424242@gmail.com>
2018-07-29 12:56:10 +0200

    Tests auf fachliche Klassen aufgeteilt
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/fluentbuilder/statechart/domain/QueueTest.java
+++ src/test/java/com/github/funthomas424242/rades/fluentbuilder/statechart/domain/QueueTest.java
@@ -38,34 +38,33 @@ import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 
-public class StatechartTest {
+public class QueueTest {
 
     @Test
-    public void createStatechartStatechart() throws IOException {
-
-        final String id = "com.github.funthomas424242.rades.fluentbuilder.test.AbstractStatechartFluentBuilder";
+    public void createQueueStatechart() throws IOException {
+        final String id = "com.github.funthomas424242.rades.fluentbuilder.test.QueueStatechart";
         final StatechartAccessor statechart = StatechartFluentBuilder.newStatechart()
             .withQualifiedClassName(id)
-            .addState("Zustand 1")
-            .addState("Zustand 2")
-            .addState("Zustand 3")
-            .withStartState("Zustand 1")
-            .addTransition("Zustand 1", "Zustand 2", "withQualifiedClassName", ParameterSignaturType.of(String.class))
+            .addState("Empty")
+            .addState("Not Empty")
+            .withStartState("Empty")
+            .addTransition("Empty", "Not Empty", "enqueue")
+            .addEmission("Empty", "isEmpty", ParameterSignaturType.of(boolean.class))
 
-            .addTransition("Zustand 2", "Zustand 2", "addState", ParameterSignaturType.of(String.class))
-            .addTransition("Zustand 2", "Zustand 3", "withStartState", ParameterSignaturType.of(String.class))
+            .addTransition("Not Empty", "Not Empty", "enqueue")
+            .addEmission("Not Empty", "isEmpty", ParameterSignaturType.of(boolean.class))
+            .addTransition("Not Empty", "Not Empty", "dequeue")
 
-            .addTransition("Zustand 3", "Zustand 3", "addTransition",
-                ParameterSignaturType.of(String.class), ParameterSignaturType.of(String.class), ParameterSignaturVararg.of("parameterSignaturs", ParameterSignatur[].class))
+            // Nichtdeterminismus nicht möglich mit FluentBuilder, da nur 1 Returntyp supported
+            //.addTransition("Not Empty", "Empty", "dequeue")
 
-            .addTransition("Zustand 3", "Zustand 3", "addEmission",
-                ParameterSignaturType.of(String.class), ParameterSignaturType.of("emissionName", String.class), ParameterSignaturType.of(Class.class))
-            .addEmission("Zustand 3", "build", ParameterSignaturType.of(Statechart.class))
-            .addEmission("Zustand 3", "build",ParameterSignaturTypeVariable.of("A"),
-                ParameterSignaturParameterizedType.of("accessorClass",Class.class,ParameterSignaturTypeVariable.of( "A")))
             .build(StatechartAccessor.class);
 
-        assertEquals(3, statechart.states().count());
+        assertEquals(2, statechart.states().count());
+        assertSame(statechart.getState("Empty"), statechart.getStartState());
+        assertEquals(statechart.getState("Not Empty"), State.of("Not Empty"));
+        assertNotSame(statechart.getState("Not Empty"), State.of("Not Empty"));
+        assertNotEquals(statechart.getState("Not Empty"), statechart.getState("Empty"));
 
         final AbstractFluentBuilderGenerator generator = new AbstractFluentBuilderGenerator(statechart);
         generator.generate("target/generated-test-sources/test-annotations/");

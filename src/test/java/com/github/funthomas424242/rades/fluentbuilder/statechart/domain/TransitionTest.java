5a536ef94b4179abc0b96400f22df78233930d9a
FunThomas424242 <funthomas424242@gmail.com>
2018-07-31 21:57:09 +0200

    Codacy issue fixed
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/test/java/com/github/funthomas424242/rades/fluentbuilder/statechart/domain/TransitionTest.java
+++ src/test/java/com/github/funthomas424242/rades/fluentbuilder/statechart/domain/TransitionTest.java
@@ -10,12 +10,12 @@ package com.github.funthomas424242.rades.fluentbuilder.statechart.domain;
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -23,7 +23,6 @@ package com.github.funthomas424242.rades.fluentbuilder.statechart.domain;
  */
 
 import com.github.funthomas424242.rades.fluentbuilder.lib.streaming.Counter;
-import com.github.funthomas424242.rades.fluentbuilder.statechart.modelling.ParameterSignaturs;
 import com.github.funthomas424242.rades.fluentbuilder.statechart.modelling.ParameterSignatursAccessor;
 import org.junit.jupiter.api.Test;
 
@@ -36,23 +35,23 @@ public class TransitionTest {
     @Test
     public void createValidInstanz() {
 
-        final Transition transition = Transition.of("Empty","Not Empty","enqueue", Integer.class, String.class);
+        final Transition transition = Transition.of("Empty", "Not Empty", "enqueue", Integer.class, String.class);
 
         assertNotNull(transition);
-        assertEquals("Empty",transition.startState.stateName);
-        assertEquals("Not Empty",transition.targetState.stateName);
-        assertEquals("enqueue",transition.transitionName);
+        assertEquals("Empty", transition.startState.stateName);
+        assertEquals("Not Empty", transition.targetState.stateName);
+        assertEquals("enqueue", transition.transitionName);
 
         final ParameterSignatursAccessor signatur = new ParameterSignatursAccessor(transition.parameterSignatur);
-        assertEquals(2,signatur.getParameterList().size());
+        assertEquals(2, signatur.getParameterList().size());
 
         final Counter counter = new Counter();
         signatur.getParameterList().stream().forEachOrdered(parameterSignatur -> {
             counter.value++;
-            if( counter.value == 1) {
+            if (counter.value == 1) {
                 assertEquals(Integer.class.getName(), parameterSignatur.getParameterTypAsTypeName().toString());
             }
-            if( counter.value == 2) {
+            if (counter.value == 2) {
                 assertEquals(String.class.getName(), parameterSignatur.getParameterTypAsTypeName().toString());
             }
         });

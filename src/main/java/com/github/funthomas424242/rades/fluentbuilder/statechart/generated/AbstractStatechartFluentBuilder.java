c3806acee72d92a447b723f0950a1cfed00f14a8
FunThomas424242 <funthomas424242@gmail.com>
2018-08-05 16:04:29 +0200

    Neugenerierung aufgenommen
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rades/fluentbuilder/statechart/generated/AbstractStatechartFluentBuilder.java
+++ src/main/java/com/github/funthomas424242/rades/fluentbuilder/statechart/generated/AbstractStatechartFluentBuilder.java
@@ -26,32 +26,31 @@ import com.github.funthomas424242.rades.fluentbuilder.statechart.domain.Statecha
 import com.github.funthomas424242.rades.fluentbuilder.statechart.modelling.ParameterSignatur;
 
 public interface AbstractStatechartFluentBuilder {
+  interface Zustand2 {
+    Zustand3 withStartState(final String startStateName);
 
-    interface Zustand2 {
-        Zustand2 addState(final String stateName);
+    Zustand2 addState(final String stateName);
+  }
 
-        Zustand3 withStartState(final String startStateName);
-    }
+  interface Zustand1 {
+    Zustand2 withQualifiedClassName(final String chartId);
+  }
 
-    interface Zustand1 {
-        Zustand2 withQualifiedClassName(final String chartId);
-    }
+  interface Zustand3 {
+    Zustand3 addEmission(final String srcStateName, final String emissionName,
+        final ParameterSignatur returnType, final ParameterSignatur... parameterSignaturs);
 
-    interface Zustand3 {
-        Statechart build();
+    Statechart build();
 
-        Zustand3 addTransition(final String srcStateName, final String targetStateName,
-                               final String transitionName, final ParameterSignatur... parameterSignaturs);
+    <A> A build(final Class<A> accessorClass);
 
-        Zustand3 addEmission(final String srcStateName, final String emissionName,
-                             final ParameterSignatur returnType);
+    Zustand3 addTransition(final String srcStateName, final String targetStateName,
+        final String transitionName, final ParameterSignatur... parameterSignaturs);
 
-        Zustand3 addEmission(final String srcStateName, final String emissionName,
-                             final ParameterSignatur returnType, final ParameterSignatur... parameterSignaturs);
+    Zustand3 addEmission(final String srcStateName, final String emissionName,
+        final ParameterSignatur returnType);
+  }
 
-        <A> A build(final Class<A> accessorClass);
-    }
-
-    interface AllStates extends Zustand2, Zustand1, Zustand3 {
-    }
+  interface AllStates extends Zustand2, Zustand1, Zustand3 {
+  }
 }

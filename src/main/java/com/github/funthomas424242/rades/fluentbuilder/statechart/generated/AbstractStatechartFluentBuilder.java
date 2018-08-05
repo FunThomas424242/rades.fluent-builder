bb8e13d78e60dd3c9b694061a38adda462d3ada4
FunThomas424242 <funthomas424242@gmail.com>
2018-08-05 19:17:42 +0200

    Neugenerierung durhgeführt
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rades/fluentbuilder/statechart/generated/AbstractStatechartFluentBuilder.java
+++ src/main/java/com/github/funthomas424242/rades/fluentbuilder/statechart/generated/AbstractStatechartFluentBuilder.java
@@ -1,35 +1,21 @@
 package com.github.funthomas424242.rades.fluentbuilder.statechart.generated;
 
-/*-
- * #%L
- * rades.fluent-builder
- * %%
- * Copyright (C) 2018 PIUG
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- *
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
- */
-
 import com.github.funthomas424242.rades.fluentbuilder.statechart.domain.Statechart;
 import com.github.funthomas424242.rades.fluentbuilder.statechart.modelling.ParameterSignatur;
+import java.lang.Class;
+import java.lang.String;
+import javax.annotation.Generated;
 
+@Generated(
+    value = "com.github.funthomas424242.rades.fluentbuilder.statechart.generators.AbstractFluentBuilderGenerator",
+    date = "2018-08-05T19:07:28.907",
+    comments = "TODO: com.github.funthomas424242.rades.fluentbuilder.statechart.generated.AbstractStatechartFluentBuilder"
+)
 public interface AbstractStatechartFluentBuilder {
   interface Zustand2 {
-    Zustand3 withStartState(final String startStateName);
-
     Zustand2 addState(final String stateName);
+
+    Zustand3 withStartState(final String startStateName);
   }
 
   interface Zustand1 {
@@ -40,12 +26,12 @@ public interface AbstractStatechartFluentBuilder {
     Zustand3 addEmission(final String srcStateName, final String emissionName,
         final ParameterSignatur returnType, final ParameterSignatur... parameterSignaturs);
 
+    Zustand3 addTransition(final String srcStateName, final String targetStateName,
+        final String transitionName, final ParameterSignatur... parameterSignaturs);
+
     Statechart build();
 
     <A> A build(final Class<A> accessorClass);
-
-    Zustand3 addTransition(final String srcStateName, final String targetStateName,
-        final String transitionName, final ParameterSignatur... parameterSignaturs);
 
     Zustand3 addEmission(final String srcStateName, final String emissionName,
         final ParameterSignatur returnType);

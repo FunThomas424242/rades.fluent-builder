27ad4bcfe2ed965c359c266302d0a5dc1283ccb6
FunThomas424242 <funthomas424242@gmail.com>
2018-08-05 20:12:58 +0200

    Neu generiert
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- src/main/java/com/github/funthomas424242/rades/fluentbuilder/statechart/generated/AbstractStatechartFluentBuilder.java
+++ src/main/java/com/github/funthomas424242/rades/fluentbuilder/statechart/generated/AbstractStatechartFluentBuilder.java
@@ -2,13 +2,11 @@ package com.github.funthomas424242.rades.fluentbuilder.statechart.generated;
 
 import com.github.funthomas424242.rades.fluentbuilder.statechart.domain.Statechart;
 import com.github.funthomas424242.rades.fluentbuilder.statechart.modelling.ParameterSignatur;
-import java.lang.Class;
-import java.lang.String;
 import javax.annotation.Generated;
 
 @Generated(
     value = "com.github.funthomas424242.rades.fluentbuilder.statechart.generators.AbstractFluentBuilderGenerator",
-    date = "2018-08-05T19:41:20.070",
+    date = "2018-08-05T20:04:45.125",
     comments = "TODO: com.github.funthomas424242.rades.fluentbuilder.statechart.generated.AbstractStatechartFluentBuilder"
 )
 public interface AbstractStatechartFluentBuilder {
@@ -24,15 +22,15 @@ public interface AbstractStatechartFluentBuilder {
 
   interface Zustand3 {
     Zustand3 addEmission(final String srcStateName, final String emissionName,
-        final ParameterSignatur returnType, final ParameterSignatur... parameterSignaturs);
+        final ParameterSignatur returnType);
 
     Zustand3 addTransition(final String srcStateName, final String targetStateName,
         final String transitionName, final ParameterSignatur... parameterSignaturs);
 
-    Zustand3 addEmission(final String srcStateName, final String emissionName,
-        final ParameterSignatur returnType);
-
     Statechart build();
+
+    Zustand3 addEmission(final String srcStateName, final String emissionName,
+        final ParameterSignatur returnType, final ParameterSignatur... parameterSignaturs);
 
     <A> A build(final Class<A> accessorClass);
   }
